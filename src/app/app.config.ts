43a719cfc5d98644e5677be3cb6b15f7f6fdc36a
Brandon Roberts <robertsbt@gmail.com>
2024-07-28 22:24:42 -0500

    feat: add syntax highlighting with PrismJS

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideContent, withMarkdownRenderer, ContentRenderer, MarkedSetupService, MarkdownContentRendererService } from '@analogjs/content';
-
+import {
+    provideContent,
+    withMarkdownRenderer,
+    ContentRenderer,
+    MarkedSetupService,
+    MarkdownContentRendererService,
+    withHighlighter
+} from '@analogjs/content';
+import { PrismHighlighter} from '@analogjs/content/prism-highlighter';
 import { routes } from './app.routes';
 import { provideFileRouter } from '@analogjs/router';
+import 'prismjs/components/prism-typescript';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideFileRouter(),
     provideRouter(routes),
-    provideContent(withMarkdownRenderer()),
+    provideContent(withMarkdownRenderer(), withHighlighter({ useClass: PrismHighlighter})),
     { provide: ContentRenderer, useClass: MarkdownContentRendererService },
-    MarkedSetupService
+    MarkedSetupService,
   ]
 };
--- src/app/pages/hello.md
+++ src/app/pages/hello.md
@@ -4,3 +4,16 @@ slug: hello
 ---
 
 # Hello World from Markdown
+
+```ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  standalone: true,
+  template: `
+    <p>hello works!</p>
+  `
+})
+export default class HelloComponent {}
+```
