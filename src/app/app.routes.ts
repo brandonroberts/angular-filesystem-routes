875e0c898dba4239324c3aaeb309443cf562fb63
Brandon Roberts <robertsbt@gmail.com>
2024-08-07 15:32:25 -0500

    chore: add types for FileRoutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { createRoutes } from '@analogjs/router';
+import { Files, createRoutes } from '@analogjs/router';
 
-const pages = {
+const pages: Files = {
   '/src/app/pages/about.page.ts': () => import('./pages/about.page'),
   '/src/app/pages/(home).page.ts': () => import('./pages/(home).page'),
   '/src/app/pages/hello.md': () => import('./pages/hello.md').then(m => m.default)
